78d38b331d3b10ff93a9d515f8a4cf10fee19106
ohhyunkyu <uyk0928@gmail.com>
2023-04-25 16:49:28 +0900

    next.js 3강 학습 중

--- Codingangma/Next.js/nextjs-tutorial/pages/index.tsx
+++ Codingangma/Next.js/nextjs-tutorial/pages/index.tsx
@@ -2,23 +2,28 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ItemList from "./../src/component/ItemList";
+import { Divider, Header, Loader } from "semantic-ui-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); //api 호출이 끝나면 다시 false로 바꾸기
   const API_URL =
     "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
 
-  // function getData() {
-  //   Axios.get(API_URL).then((res) => {
-  //     console.log(res);
-  //   });
-  // }
+  function getData() {
+    Axios.get(API_URL).then((res) => {
+      setList(res.data);
+      setIsLoading(false);
+    });
+  }
 
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
@@ -27,17 +32,27 @@ export default function Home() {
           {" "}
           <title>HOME | 코딩앙마</title>{" "}
         </Head>
-        create-next-app <br />
-        - create-next-app
-        <br />
-        - 이를 통해 설치 시 자동으로 컴파일과 번들링이 됨(webpack과 babel)
-        <br />
-        - 자동 리프레쉬 기능으로 수정 시 화면에 바로 반영
-        <br />
-        - 서버사이드 렌더링 지원
-        <br />
-        - 스태틱 파일을 지원
-        <br />
+        {isLoading && (
+          <div style={{ padding: "300px 0" }}>
+            <Loader inline="centered" active>
+              Loading
+            </Loader>
+          </div>
+        )}
+        {!isLoading && (
+          <>
+            <Header as="h3" style={{ paddingTop: 40 }}>
+              베스트 상품
+            </Header>
+            <Divider />
+            <ItemList list={list.slice(0, 9)} />
+            <Header as="h3" style={{ paddingTop: 40 }}>
+              신상품
+            </Header>
+            <Divider />
+            <ItemList list={list.slice(9)} />
+          </>
+        )}
       </div>
     </>
   );
--- Codingangma/Next.js/nextjs-tutorial/pages/view/[id].tsx
+++ Codingangma/Next.js/nextjs-tutorial/pages/view/[id].tsx
@@ -1,3 +1,42 @@
-export default function Id() {
-  return <div>ID</div>;
-}
+import Item from "@/src/component/Item";
+import Axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Loader } from "semantic-ui-react";
+
+const Post = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+
+  function getData() {
+    Axios.get(API_URL).then((res) => {
+      setItem(res.data);
+      setIsLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    if (id && id > 0) {
+      getData();
+    }
+  }, [id]);
+
+  return (
+    <>
+      {isLoading ? (
+        <div style={{ padding: "300px 0" }}>
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>
+      ) : (
+        <Item item={item} />
+      )}
+    </>
+  );
+};
+
+export default Post;
